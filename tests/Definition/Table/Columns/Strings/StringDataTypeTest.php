d4199c31d9e08f54104687a53848b87217cd7a33
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2020-09-09 13:13:31 -0500

    Create StringDataTypeTest.php

---
+++ tests/Definition/Table/Columns/Strings/StringDataTypeTest.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ * This file is part of Aplus Framework Database Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\Database\Definition\Table\Columns\String;
+
+use Tests\Database\TestCase;
+
+final class StringDataTypeTest extends TestCase
+{
+    protected StringDataTypeMock $column;
+
+    protected function setUp() : void
+    {
+        $this->column = new StringDataTypeMock(static::$database);
+    }
+
+    public function testCharset() : void
+    {
+        self::assertSame(
+            " mock CHARACTER SET 'utf8' NOT NULL",
+            $this->column->charset('utf8')->sql()
+        );
+    }
+
+    public function testCollate() : void
+    {
+        self::assertSame(
+            " mock COLLATE 'utf8_general_ci' NOT NULL",
+            $this->column->collate('utf8_general_ci')->sql()
+        );
+    }
+
+    public function testFull() : void
+    {
+        self::assertSame(
+            " mock CHARACTER SET 'utf8' COLLATE 'utf8_general_ci' NOT NULL",
+            $this->column->collate('utf8_general_ci')->charset('utf8')->sql()
+        );
+    }
+}
