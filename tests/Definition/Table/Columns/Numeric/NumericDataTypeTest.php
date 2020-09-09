e684a988461985f373366edaa8c543cd9ea3f746
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2020-09-09 13:13:56 -0500

    Create NumericDataTypeTest.php

---
+++ tests/Definition/Table/Columns/Numeric/NumericDataTypeTest.php
@@ -0,0 +1,62 @@
+<?php
+/*
+ * This file is part of Aplus Framework Database Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\Database\Definition\Table\Columns\Numeric;
+
+use Tests\Database\TestCase;
+
+final class NumericDataTypeTest extends TestCase
+{
+    protected NumericDataTypeMock $column;
+
+    protected function setUp() : void
+    {
+        $this->column = new NumericDataTypeMock(static::$database);
+    }
+
+    public function testAutoIncrement() : void
+    {
+        self::assertSame(
+            ' mock AUTO_INCREMENT NOT NULL',
+            $this->column->autoIncrement()->sql()
+        );
+    }
+
+    public function testSigned() : void
+    {
+        self::assertSame(
+            ' mock signed NOT NULL',
+            $this->column->signed()->sql()
+        );
+    }
+
+    public function testUnsigned() : void
+    {
+        self::assertSame(
+            ' mock unsigned NOT NULL',
+            $this->column->unsigned()->sql()
+        );
+    }
+
+    public function testZerofill() : void
+    {
+        self::assertSame(
+            ' mock zerofill NOT NULL',
+            $this->column->zerofill()->sql()
+        );
+    }
+
+    public function testFull() : void
+    {
+        self::assertSame(
+            ' mock unsigned zerofill AUTO_INCREMENT NOT NULL',
+            $this->column->unsigned()->zerofill()->autoIncrement()->sql()
+        );
+    }
+}
