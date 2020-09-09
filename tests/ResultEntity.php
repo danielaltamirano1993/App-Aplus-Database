32a635e38f0a966adb3aa380240ede9c39d11048
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2020-09-09 13:02:52 -0500

    Create ResultEntity.php

---
+++ tests/ResultEntity.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * This file is part of Aplus Framework Database Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\Database;
+
+class ResultEntity
+{
+    public mixed $p1;
+    public mixed $p2;
+
+    public function __construct(mixed $p1, mixed $p2)
+    {
+        $this->p2 = $p2;
+    }
+}
