ad33a572f86a2a65e610aa380906b4bd27b42763
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2020-09-09 13:14:04 -0500

    Create NumericDataTypeMock.php

---
+++ tests/Definition/Table/Columns/Numeric/NumericDataTypeMock.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * This file is part of Aplus Framework Database Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\Database\Definition\Table\Columns\Numeric;
+
+use Framework\Database\Definition\Table\Columns\Numeric\NumericDataType;
+
+class NumericDataTypeMock extends NumericDataType
+{
+    protected string $type = 'mock';
+}
