3ad7f6f3cc51e2f103eebfd918d880bdcdace93a
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2020-09-09 13:14:44 -0500

    Create IndexMock.php

---
+++ tests/Definition/Table/Indexes/IndexMock.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * This file is part of Aplus Framework Database Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\Database\Definition\Table\Indexes;
+
+use Framework\Database\Definition\Table\Indexes\Index;
+
+class IndexMock extends Index
+{
+    public string $type = 'index_mock';
+}
