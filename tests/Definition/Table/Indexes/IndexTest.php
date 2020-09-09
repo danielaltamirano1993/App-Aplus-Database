5b2000a46c6ae259e6597ea0b8c56a30e0f898a8
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2020-09-09 13:14:34 -0500

    Create IndexTest.php

---
+++ tests/Definition/Table/Indexes/IndexTest.php
@@ -0,0 +1,55 @@
+<?php
+/*
+ * This file is part of Aplus Framework Database Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\Database\Definition\Table\Indexes;
+
+use Tests\Database\TestCase;
+
+final class IndexTest extends TestCase
+{
+    protected IndexMock $index;
+
+    protected function setUp() : void
+    {
+        $this->index = new IndexMock(static::$database, null, 'id');
+    }
+
+    public function testEmptyType() : void
+    {
+        $this->index->type = '';
+        $this->expectException(\LogicException::class);
+        $this->expectExceptionMessage('Key type is empty');
+        $this->index->sql();
+    }
+
+    public function testMultiColumns() : void
+    {
+        $index = new IndexMock(static::$database, null, 'id', 'email', 'foo');
+        self::assertSame(
+            ' index_mock (`id`, `email`, `foo`)',
+            $index->sql()
+        );
+    }
+
+    public function testName() : void
+    {
+        $index = new IndexMock(static::$database, 'foo', 'id');
+        self::assertSame(
+            ' index_mock `foo` (`id`)',
+            $index->sql()
+        );
+    }
+
+    public function testBadMethod() : void
+    {
+        $this->expectException(\BadMethodCallException::class);
+        $this->expectExceptionMessage('Method not found or not allowed: foo');
+        $this->index->foo(); // @phpstan-ignore-line
+    }
+}
