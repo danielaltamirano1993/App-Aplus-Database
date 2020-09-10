371c3c02b0a22803ae1f732644b99b4a5bd380dd
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2020-09-10 13:15:53 -0500

    Create FulltextKeyTest.php

---
+++ tests/Definition/Table/Indexes/Keys/FulltextKeyTest.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * This file is part of Aplus Framework Database Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\Database\Definition\Table\Indexes\Keys;
+
+use Framework\Database\Definition\Table\Indexes\Keys\FulltextKey;
+use Tests\Database\TestCase;
+
+final class FulltextKeyTest extends TestCase
+{
+    public function testType() : void
+    {
+        $index = new FulltextKey(static::$database, null, 'id');
+        self::assertSame(
+            ' FULLTEXT KEY (`id`)',
+            $index->sql()
+        );
+    }
+}
