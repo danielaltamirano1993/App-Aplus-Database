c181ece93cd0d2d5b412c7aba77d083bb8e82262
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2020-09-10 13:15:32 -0500

    Create PrimaryKeyTest.php

---
+++ tests/Definition/Table/Indexes/Keys/PrimaryKeyTest.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * This file is part of Aplus Framework Database Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\Database\Definition\Table\Indexes\Keys;
+
+use Framework\Database\Definition\Table\Indexes\Keys\PrimaryKey;
+use Tests\Database\TestCase;
+
+final class PrimaryKeyTest extends TestCase
+{
+    public function testType() : void
+    {
+        $index = new PrimaryKey(static::$database, null, 'id');
+        self::assertSame(
+            ' PRIMARY KEY (`id`)',
+            $index->sql()
+        );
+    }
+}
