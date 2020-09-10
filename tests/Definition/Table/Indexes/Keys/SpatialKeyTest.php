da31dbfbdac1510b6659d1f3f166c5cd8c2a17e8
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2020-09-10 13:15:23 -0500

    Create SpatialKeyTest.php

---
+++ tests/Definition/Table/Indexes/Keys/SpatialKeyTest.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * This file is part of Aplus Framework Database Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\Database\Definition\Table\Indexes\Keys;
+
+use Framework\Database\Definition\Table\Indexes\Keys\SpatialKey;
+use Tests\Database\TestCase;
+
+final class SpatialKeyTest extends TestCase
+{
+    public function testType() : void
+    {
+        $index = new SpatialKey(static::$database, null, 'id');
+        self::assertSame(
+            ' SPATIAL KEY (`id`)',
+            $index->sql()
+        );
+    }
+}
