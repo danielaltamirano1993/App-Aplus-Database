7705dfc65048012d2af9582f92f32781c943f157
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2020-09-10 13:17:47 -0500

    Create WithTest.php

---
+++ tests/Manipulation/WithTest.php
@@ -0,0 +1,105 @@
+<?php
+/*
+ * This file is part of Aplus Framework Database Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\Database\Manipulation;
+
+use Framework\Database\Manipulation\Select;
+use Framework\Database\Manipulation\With;
+use Framework\Database\Result;
+use Tests\Database\TestCase;
+
+final class WithTest extends TestCase
+{
+    protected With $with;
+
+    public function setup() : void
+    {
+        $this->with = new With(static::$database);
+    }
+
+    protected function prepareWith() : void
+    {
+        $this->with->reference(
+            't1',
+            static fn (Select $s) => $s->columns('*')->from('folks')->sql()
+        )->select(
+            static fn (Select $s) => $s->columns('*')->from('ancestors')->sql()
+        );
+    }
+
+    public function testWith() : void
+    {
+        $this->prepareWith();
+        self::assertSame(
+            "WITH\n`t1` AS (SELECT\n *\n FROM `folks`\n)\nSELECT\n *\n FROM `ancestors`\n",
+            $this->with->sql()
+        );
+    }
+
+    public function testOptions() : void
+    {
+        $this->prepareWith();
+        $this->with->options($this->with::OPT_RECURSIVE);
+        self::assertSame(
+            "WITH\nRECURSIVE\n`t1` AS (SELECT\n *\n FROM `folks`\n)\nSELECT\n *\n FROM `ancestors`\n",
+            $this->with->sql()
+        );
+    }
+
+    public function testManyReferences() : void
+    {
+        $this->prepareWith();
+        $this->with->reference('t2', static fn () => 'select * from foo');
+        self::assertSame(
+            "WITH\n`t1` AS (SELECT\n *\n FROM `folks`\n), `t2` AS (select * from foo)\nSELECT\n *\n FROM `ancestors`\n",
+            $this->with->sql()
+        );
+    }
+
+    public function testInvalidOption() : void
+    {
+        $this->with->options('foo');
+        $this->expectException(\LogicException::class);
+        $this->expectExceptionMessage('Invalid option: foo');
+        $this->with->sql();
+    }
+
+    public function testWithoutReference() : void
+    {
+        $this->expectException(\LogicException::class);
+        $this->expectExceptionMessage('References must be set');
+        $this->with->sql();
+    }
+
+    public function testWithoutSelect() : void
+    {
+        $this->with->reference(
+            't1',
+            static fn (Select $s) => $s->columns('*')->from('folks')->sql()
+        );
+        $this->expectException(\LogicException::class);
+        $this->expectExceptionMessage('SELECT must be set');
+        $this->with->sql();
+    }
+
+    public function testRun() : void
+    {
+        $this->createDummyData();
+        $this->with->reference(
+            't1',
+            static fn (Select $s) => $s->columns('*')->from('t1')->sql()
+        )->select(
+            static fn (Select $s) => $s->columns('*')->from('t2')->sql()
+        );
+        self::assertInstanceOf(
+            Result::class,
+            $this->with->run()
+        );
+    }
+}
