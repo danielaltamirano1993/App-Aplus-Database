f2c02840b454e9570a8d0d582456a4797b059e79
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2020-09-10 13:18:20 -0500

    Create StatementMock.php

---
+++ tests/Manipulation/StatementMock.php
@@ -0,0 +1,70 @@
+<?php
+/*
+ * This file is part of Aplus Framework Database Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\Database\Manipulation;
+
+use Closure;
+use Framework\Database\Manipulation\Statement;
+
+class StatementMock extends Statement
+{
+    public function subquery(Closure $subquery) : string
+    {
+        return parent::subquery($subquery);
+    }
+
+    /**
+     * @param int $limit
+     * @param int|null $offset
+     *
+     * @return static
+     */
+    public function limit(int $limit, int $offset = null) : static
+    {
+        return $this->setLimit($limit, $offset);
+    }
+
+    public function renderLimit() : ?string
+    {
+        return parent::renderLimit();
+    }
+
+    public function renderIdentifier(Closure | string $column) : string
+    {
+        return parent::renderIdentifier($column);
+    }
+
+    public function renderAliasedIdentifier(array | Closure | string $column) : string
+    {
+        return parent::renderAliasedIdentifier($column);
+    }
+
+    public function renderAssignment(string $identifier, $expression) : string
+    {
+        return parent::renderAssignment($identifier, $expression);
+    }
+
+    public function renderOptions() : ?string
+    {
+        if ( ! $this->hasOptions()) {
+            return null;
+        }
+        return \implode(' ', $this->sql['options']);
+    }
+
+    public function sql() : string
+    {
+        return 'SQL';
+    }
+
+    public function run() : mixed
+    {
+        return null;
+    }
+}
