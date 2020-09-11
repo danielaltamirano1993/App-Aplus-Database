a867436226f58766e911cf4d795c312f72b81e77
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2020-09-11 13:20:00 -0500

    Create WhereMock.php

---
+++ tests/Manipulation/Traits/WhereMock.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * This file is part of Aplus Framework Database Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\Database\Manipulation\Traits;
+
+use Framework\Database\Manipulation\Traits\Where;
+use Tests\Database\Manipulation\StatementMock;
+
+class WhereMock extends StatementMock
+{
+    use Where {
+        renderWhere as public;
+    }
+}
