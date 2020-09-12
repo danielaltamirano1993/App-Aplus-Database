d11434136cde234c15e8c0f0d6046a753c9ce95c
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2020-09-12 13:20:46 -0500

    Create JoinMock.php

---
+++ tests/Manipulation/Traits/JoinMock.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * This file is part of Aplus Framework Database Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\Database\Manipulation\Traits;
+
+use Framework\Database\Manipulation\Traits\Join;
+use Tests\Database\Manipulation\StatementMock;
+
+class JoinMock extends StatementMock
+{
+    use Join {
+        hasFrom as public;
+        renderFrom as public;
+        renderJoin as public;
+    }
+}
