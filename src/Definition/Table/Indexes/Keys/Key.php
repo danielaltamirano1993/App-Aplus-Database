e726017f371f85da5f1056b9d15f664de774bafe
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2020-09-16 13:31:51 -0500

    Create Key.php

---
+++ src/Definition/Table/Indexes/Keys/Key.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of Aplus Framework Database Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Framework\Database\Definition\Table\Indexes\Keys;
+
+use Framework\Database\Definition\Table\Indexes\Index;
+
+/**
+ * Class Key.
+ *
+ * @package database
+ */
+final class Key extends Index
+{
+    protected string $type = 'KEY';
+}
