5298feba32a7d5c842786751a153fab42545670e
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2020-09-17 14:05:42 -0500

    Create MediumtextColumn.php

---
+++ src/Definition/Table/Columns/String/MediumtextColumn.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of Aplus Framework Database Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Framework\Database\Definition\Table\Columns\String;
+
+/**
+ * Class MediumtextColumn.
+ *
+ * @see https://mariadb.com/kb/en/mediumtext/
+ *
+ * @package database
+ */
+final class MediumtextColumn extends StringDataType
+{
+    protected string $type = 'mediumtext';
+}
