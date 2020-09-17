87686e716188e376d7388658944e5c359e1c77ab
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2020-09-17 14:05:33 -0500

    Create SetColumn.php

---
+++ src/Definition/Table/Columns/String/SetColumn.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of Aplus Framework Database Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Framework\Database\Definition\Table\Columns\String;
+
+use Framework\Database\Definition\Table\Columns\Traits\ListLength;
+
+/**
+ * Class SetColumn.
+ *
+ * @see https://mariadb.com/kb/en/set-data-type/
+ *
+ * @package database
+ */
+final class SetColumn extends StringDataType
+{
+    use ListLength;
+    protected string $type = 'set';
+}
