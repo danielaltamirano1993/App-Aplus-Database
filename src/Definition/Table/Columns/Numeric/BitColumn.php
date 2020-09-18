0a79edec3b15dac72e1a9982767914b1a3a6e37d
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2020-09-18 14:14:37 -0500

    Create BitColumn.php

---
+++ src/Definition/Table/Columns/Numeric/BitColumn.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of Aplus Framework Database Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Framework\Database\Definition\Table\Columns\Numeric;
+
+use Framework\Database\Definition\Table\Columns\Column;
+
+/**
+ * Class BitColumn.
+ *
+ * @see https://mariadb.com/kb/en/bit/
+ *
+ * @package database
+ */
+final class BitColumn extends Column
+{
+    protected string $type = 'bit';
+}
