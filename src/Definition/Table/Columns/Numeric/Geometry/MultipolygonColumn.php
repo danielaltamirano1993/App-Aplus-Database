2844ed34ef9e5c97370a481ae81c1486b22d0537
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2020-09-19 14:20:30 -0500

    Create MultipolygonColumn.php

---
+++ src/Definition/Table/Columns/Numeric/Geometry/MultipolygonColumn.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of Aplus Framework Database Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Framework\Database\Definition\Table\Columns\Geometry;
+
+use Framework\Database\Definition\Table\Columns\Column;
+
+/**
+ * Class MultipolygonColumn.
+ *
+ * @see https://mariadb.com/kb/en/multipolygon/
+ *
+ * @package database
+ */
+final class MultipolygonColumn extends Column
+{
+    protected string $type = 'multipolygon';
+}
